d208045cbc37eb5b278271a74c02cc7c45d34718
S00163419 <S00163419@mail.itsligo.ie>
2017-12-01 15:17:09 +0000

    Part 2, 3 and 4 attempted, added new controller, database still blocked. one known error.

---
+++ Rad301ClubsV1/Controllers/ClubMembers.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rad301ClubsV1.Models.ClubModel;
+using Rad301ClubsV1.Models;
+
+namespace Rad301ClubsV1.Controllers
+{
+    [Authorize(Roles = "Admin,ClubAdmin")]
+    public class ClubMembers : Controller
+    {
+        private ClubContext db = new ClubContext();
+
+        // GET: ClubMembers
+        public async Task<ActionResult> Index()
+
+        {
+
+            using (ApplicationDbContext adb = new ApplicationDbContext())
+            {
+                var adminuser = GetApplicationUsersInRole(adb, User.Identity.Name).FirstOrDefault();
+            }
+
+            var ClubMembers = db.Clubs.Include("Clubmembers").ToList();
+            return View(await db.Clubs.ToListAsync());
+        }
+
+
+        public IEnumerable<ApplicationUser> GetApplicationUsersInRole(ApplicationDbContext context, string userName)
+        {
+            return from role in context.Roles
+                       //  where role.Name == roleName
+                   from userRoles in role.Users
+                   join user in context.Users
+                   on userRoles.UserId equals user.Id
+                   where user.UserName == userName
+                   select user;
+        }
+
+
+        // GET: ClubMembers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = await db.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // GET: ClubMembers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClubMembers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ClubId,ClubName,CreationDate,adminID")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clubs.Add(club);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(club);
+        }
+
+        // GET: ClubMembers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = await db.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: ClubMembers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ClubId,ClubName,CreationDate,adminID")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(club).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(club);
+        }
+
+        // GET: ClubMembers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = await db.Clubs.FindAsync(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: ClubMembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Club club = await db.Clubs.FindAsync(id);
+            db.Clubs.Remove(club);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
