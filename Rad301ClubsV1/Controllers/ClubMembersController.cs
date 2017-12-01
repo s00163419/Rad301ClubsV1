046fbad3dfb3a86078e4f1ceca85d9e4da534171
S00163419 <S00163419@mail.itsligo.ie>
2017-12-01 14:05:35 +0000

    Part 2, 3, 4 attempted, cannot open database, port may be locked?

---
+++ Rad301ClubsV1/Controllers/ClubMembersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rad301ClubsV1.Controllers
+{
+    public class ClubMembersController : Controller
+    {
+
+        [Authorize(Roles = "ClubAdmin")]
+
+        // GET: ClubMembers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
